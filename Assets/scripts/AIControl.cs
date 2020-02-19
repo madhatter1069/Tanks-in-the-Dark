0569708167bf9581f16cd9e83092bad02037ff17
Jared <jaredclark11@gmail.com>
2020-02-18 20:14:21 -0800

    ai start

---
+++ Assets/scripts/AIControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIControl : MonoBehaviour
+{
+    public team teamColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
